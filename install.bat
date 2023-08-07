8b72e01ada11c9a9122617b385493b75882e61e6
ngn <78868991+ngn13@users.noreply.github.com>
2023-08-07 14:17:20 +0300

    removed install.bat

--- install.bat
+++
@@ -1,9 +0,0 @@
-mkdir build
-pip -r requirements.txt
-echo "Copying 'src/config.py' to 'build/config.pyw'"
-copy src/config.py build/config.pyw
-echo "Copying 'src/presence.py' to 'build/presence.pyw'"
-copy src/presence.py build/presence.pyw
-echo "Copying 'src/config.ui' to 'build/config.ui'"
-copy src/config.ui build/config.ui
-echo "Done!"
